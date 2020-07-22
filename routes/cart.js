a300110ea22e5f7ec9aa0a0142c854f27b3b9ee3
Evgeniy Kapelko <eugene.kapelko@balanceinternet.com.au>
2020-07-22 17:56:14 +0300

    Refactoring Cart model

--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,13 @@
 const {Router} = require('express');
 const router = require('./courses');
-const Cart = require('../models/cart');
 const Course = require('../models/course');
 
 const routr = new Router();
 
 
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Cart.add(course)
+    const course = await Course.findById(req.body.id)
+    await req.user.addToCart(course);
     res.redirect('/cart')
 })
 
