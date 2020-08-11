b242b7738a0dc30ebe9cbbae2b767ae79d29fea1
Evgeniy Kapelko <eugene.kapelko@balanceinternet.com.au>
2020-08-11 17:15:52 +0300

    Save session in db

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
@@ -15,27 +16,26 @@ const config = require('./config');
 const User = require('./models/user');
 const varMiddleware = require('./middleware/variables')
 
-const app = express();
+const url = sprintf(
+    config.MONGO_URL,
+    config.NONGO_USER,
+    config.MONGO_PASSWORD,
+    config.MONGO_DB
+)
 
+const app = express();
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
+})
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: url
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
-
-
-// app.use(async (req, res, next) => {
-//     try {
-//         const user = await User.findById('54544gft54gssdfs')
-//         req.user = user
-//         next()
-//     } catch (e) {
-//         console.log(e)
-//     }
-// })
 
 // for css 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -45,7 +45,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(session({
     secret: 'some secret value',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }))
 app.use(varMiddleware)
 
@@ -60,29 +61,12 @@ app.use('/auth', authRoutes)
 async function start() {
     try {
         const PORT = config.PORT;
-        const url = sprintf(
-            config.MONGO_URL,
-            config.NONGO_USER,
-            config.MONGO_PASSWORD,
-            config.MONGO_DB
-        )
-
+        
         await mongoose.connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
         })
-
-        // const candidate = await User.findOne()
-        // if (!candidate) {
-        //     const user = new User({
-        //         email: 'admin@admin.com',
-        //         name: 'Jenya',
-        //         cart: {item: []}
-        //     })
-
-        //     await user.save()
-        // }
         app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)})
     } catch(e) {
         console.log(e)
