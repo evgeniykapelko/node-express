745f5fc8b2ca12757d109091fc90eb42bb9eeb8e
Eugene Kapelko <eugene.kapelko@balanceinternet.com.au>
2020-08-26 17:35:54 +0300

    Change password to hash

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const router = Router()
 
@@ -22,7 +23,7 @@ router.post('/login', async (req, res) => {
         const candidate = await User.findOne({ email })
 
         if (candidate) {
-            const areSame = password === candidate.password 
+            const areSame = await bcrypt.compare(password, candidate.password)
 
             if (areSame) {
                 req.session.user = candidate;
@@ -53,8 +54,9 @@ router.post('/register', async (req, res) => {
         if (candidate) {
             res.redirect('/auth/login#register')
         } else {
+            const hashPassword = await bcrypt.hash(password, 10)
             const user = new User({
-                email, name, password, cart: {items: []}
+                email, name, hashPassword, cart: {items: []}
             })
 
             await user.save()
